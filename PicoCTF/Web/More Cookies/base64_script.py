c69e58cc730fbe130d83790a22c7cf0d768dfd03
SBK <48666130+bernie6401@users.noreply.github.com>
2023-02-20 19:38:28 +0800

    update picoctf files

--- PicoCTF/Web/More Cookies/base64_script.py
+++ PicoCTF/Web/More Cookies/base64_script.py
@@ -1,32 +1,19 @@
 import base64
 import requests
+from tqdm import trange
 
 s = requests.session()
 url = 'http://mercury.picoctf.net:43275/'
 s.get(url)
 cookie = s.cookies['auth_name']
 b64_cookie = base64.b64decode(cookie)
-b64_cookie = base64.b64decode(b64_cookie).decode()
-# print()
-for i in range(0, 128):
-    pos = i // 8
-    guessdec = b64_cookie[0:pos] + chr(ord(b64_cookie[pos])^(1 << (i%8))) + b64_cookie[pos+1:]
-    guessenc1 = base64.b64encode(guessdec)
+b64_cookie = base64.b64decode(b64_cookie)
 
-    guess = base64.b64encode(base64.b64encode(guessdec))
-
-    r = requests.get(url, cookies={"auth_name":guess})
-
+for i in trange (0,128):    # When i==72, we'll get the flag
+    pos=i//8
+    guessdec=b64_cookie[0:pos]+((b64_cookie[pos]^(1 << (i%8))).to_bytes(1,'big'))+b64_cookie[pos+1:]
+    guess=base64.b64encode(base64.b64encode(guessdec)).decode()
+    r=requests.get(url,cookies={"auth_name": guess})
     if "pico" in r.text:
         print(r.text)
-
-
-# file = open('./output.txt', 'r')
-# # b64_file = open('./base64_output.txt', 'a')
-# for f in file.readlines():
-#     print(type(f))
-#     with open('./base64_output.txt', "a") as b64_file:
-#         b64_file.write(base64.b64decode(f))
-
-# b64_file.close()
-# file.close()
+        print(guess)
