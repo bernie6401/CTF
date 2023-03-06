ab2afb20a69f84ec36c0fe5f1f2f04e37124ecc0
SBK <48666130+bernie6401@users.noreply.github.com>
2023-03-06 15:44:54 +0800

    add pwn problem of picoctf

---
+++ PicoCTF/PWN/Stonks/test.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+    system("date"); // TODO: implement this in C
+    // fflush(stdout);
+}
