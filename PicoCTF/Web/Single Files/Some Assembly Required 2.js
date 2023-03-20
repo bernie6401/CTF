b81d0e4b6476de12e62d7c95dd8df594638c537f
SBK <48666130+bernie6401@users.noreply.github.com>
2023-03-20 19:16:14 +0800

    update single files

---
+++ PicoCTF/Web/Single Files/Some Assembly Required 2.js
@@ -0,0 +1,36 @@
+const dic_str = ['copy_char', 'value', '207aLjBod', '1301420SaUSqf', '233ZRpipt', '2224QffgXU', 'check_flag', '408533hsoVYx', 'instance', '278338GVFUrH', 'Correct!', '549933ZVjkwI', 'innerHTML', 'charCodeAt', './aD8SvhyVkb', 'result', '977AzKzwq', 'Incorrect!', 'exports', 'length', 'getElementById', '1jIrMBu', 'input', '615361geljRK'];
+const dic_str_chosen = function(val1, _0x4d6e6c) {
+    val1 = val1 - 195;
+    let dic_strc4 = dic_str[val1];
+    return dic_strc4;
+};
+(function(_0x12fd07, _0x4e9d05) {
+    const dic_str_chosen = dic_str_chosen;
+    while (!![]) {
+        try {
+            const key = -parseInt(dic_str_chosen(200)) * -parseInt(dic_str_chosen(201)) + -parseInt(dic_str_chosen(205)) + parseInt(dic_str_chosen(207)) + parseInt(dic_str_chosen(0xc3)) + -parseInt(dic_str_chosen(0xc6)) * parseInt(dic_str_chosen(0xd4)) + parseInt(dic_str_chosen(0xcb)) + -parseInt(dic_str_chosen(0xd9)) * parseInt(dic_str_chosen(0xc7));
+            if (key === _0x4e9d05) break;
+            else _0x12fd07['push'](_0x12fd07['shift']());
+        }
+        catch (_0x4f8a) {
+            _0x12fd07['push'](_0x12fd07['shift']());
+        }
+    }
+}(dic_str, 310022));
+let exports;
+(async () => {
+    const dic_str_chosen = dic_str_chosen;
+    let _0x1adb5f = await fetch(dic_str_chosen(210)),
+        _0x355961 = await WebAssembly['instantiate'](await _0x1adb5f['arrayBuffer']()),
+        _0x5c0ffa = _0x355961[dic_str_chosen(204)];
+    exports = _0x5c0ffa[dic_str_chosen(214)];
+})();
+
+function onButtonPress() {
+    const dic_str_chosen = dic_str_chosen;
+    let _0x5f4170 = document[dic_str_chosen(216)](dic_str_chosen(0xda))[dic_str_chosen(197)];
+    for (let _0x19d3ca = 0; _0x19d3ca < _0x5f4170['length']; _0x19d3ca++) {
+        exports[dic_str_chosen(196)](_0x5f4170[dic_str_chosen(209)](_0x19d3ca), _0x19d3ca);
+    }
+    exports['copy_char'](0, _0x5f4170[dic_str_chosen(215)]), exports[dic_str_chosen(202)]() == 1 ? document['getElementById'](dic_str_chosen(211))[dic_str_chosen(208)] = dic_str_chosen(206) : document[dic_str_chosen(216)](dic_str_chosen(211))['innerHTML'] = dic_str_chosen(0xd5);
+}
