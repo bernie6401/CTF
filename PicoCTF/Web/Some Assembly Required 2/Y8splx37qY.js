f2aab6c2b20c0920f6ad2a85774b42b302244685
bernie6401 <bernie6401@gmail.com>
2023-06-20 11:37:41 +0800

    update web files

---
+++ PicoCTF/Web/Some Assembly Required 2/Y8splx37qY.js
@@ -0,0 +1,35 @@
+const list_str = ['copy_char', 'value', '207aLjBod', '1301420SaUSqf', '233ZRpipt', '2224QffgXU', 'check_flag', '408533hsoVYx', 'instance', '278338GVFUrH', 'Correct!', '549933ZVjkwI', 'innerHTML', 'charCodeAt', './aD8SvhyVkb', 'result', '977AzKzwq', 'Incorrect!', 'exports', 'length', 'getElementById', '1jIrMBu', 'input', '615361geljRK'];
+const func1_cal = function(var_a, var_b) {
+    var_a = var_a - 195;
+    let list_strc4 = list_str[var_a];
+    return list_strc4;
+};
+(function(var_a, var_b) {
+    const func1_cal = func1_cal;
+    while (!![]) {
+        try {
+            const var_c = -parseInt(func1_cal(200)) * -parseInt(func1_cal(201)) + -parseInt(func1_cal(205)) + parseInt(func1_cal(207)) + parseInt(func1_cal(195)) + -parseInt(func1_cal(198)) * parseInt(func1_cal(212)) + parseInt(func1_cal(203)) + -parseInt(func1_cal(217)) * parseInt(func1_cal(199));
+            if (var_c === var_b) break;
+            else var_a['push'](var_a['shift']());
+        } catch (_0x4f8a) {
+            var_a['push'](var_a['shift']());
+        }
+    }
+}(list_str, 310022));
+let exports;
+(async () => {
+    const func1_cal = func1_cal;
+    let res_1 = await fetch(func1_cal(210)),
+        res_2 = await WebAssembly['instantiate'](await res_1['arrayBuffer']()),
+        res_3 = res_2[func1_cal(204)];
+    exports = res_3[func1_cal(214)];
+})();
+
+function onButtonPress() {
+    const func1_cal = func1_cal;
+    let res_1 = document[func1_cal(216)](func1_cal(218))[func1_cal(197)];
+    for (let idx = 0; idx < res_1['length']; idx++) {
+        exports[func1_cal(196)](res_1[func1_cal(209)](idx), idx);
+    }
+    exports['copy_char'](0, res_1[func1_cal(215)]), exports[func1_cal(202)]() == 1 ? document['getElementById'](func1_cal(211))[func1_cal(208)] = func1_cal(206) : document[func1_cal(216)](func1_cal(211))['innerHTML'] = func1_cal(213);
+}
